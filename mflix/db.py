0cc86365cab5b6531d592538785dbb4cb670f7f6
Harsh Agarwal <agarwalharsh244@gmail.com>
2020-11-03 08:51:13 +0530

    Ticket:Paging
    scrolling to the bottom of the page will result in a new page of movies.

--- mflix/db.py
+++ mflix/db.py
@@ -235,7 +235,10 @@ def get_movies(filters, page, movies_per_page):
 
     # TODO: Paging
     # Use the cursor to only return the movies that belong on the current page.
-    movies = cursor.limit(movies_per_page)
+    if page==0:
+        movies = cursor.limit(movies_per_page)
+    else:
+        movies=cursor.skip(int(page)*int(movies_per_page)).limit(movies_per_page)
 
     return (list(movies), total_num_movies)
 
